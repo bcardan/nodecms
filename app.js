1d59325b812ba3260705f991e826689b65e2a149
Bent Cardan <bent@kickr.io>
2012-09-29 22:52:21 -0400

    adding styles and photos

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+  , photos = require('./routes/photos')
   , http = require('http')
   , path = require('path')
   , md = require('github-flavored-markdown').parse
@@ -46,9 +47,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
-app.get('/feed', function(){
-  res.render('rss.ejs');
-});
+app.get('/photos', photos.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/photos.js
+++ routes/photos.js
@@ -14,3 +14,10 @@ photos.push({
 	name: 'three',
 	path: '3.png'
 });
+
+exports.list = function(req, res){
+  res.render('photos', {
+    title: 'Photos',
+    photos: photos
+  });
+};
